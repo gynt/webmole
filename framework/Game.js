d134f7c0fa887784c330ae37ad77acca55e170fd
gynt <gynt@users.noreply.github.com>
2018-02-06 16:40:13 +0100

    Update Game.js

--- framework/Game.js
+++ framework/Game.js
@@ -1,3 +1,29 @@
+const Round = require('./Round');
+
 function Game() {
-  
+  this.rounds=new Array(new Round());
+  this.players=new Array();
+}
+
+Game.prototype.pickMole = function() {
+  this.rounds[0].players.forEach(function(el, i){
+    el.is_mole=false;
+  });
+  this.rounds[0].players[getRandomInt(this.rounds[0].players.length)].is_mole=true;
+}
+
+Game.prototype.hasMole = function() {
+  return this.rounds[0].players.filter(function(el,i){
+    return el.is_mole==true;
+  }).length > 0;
+}
+
+Game.prototype.lookupPlayer = function(id) {
+  return this.players.filter((el) => el.id==id)[0];
+}
+
+module.exports = Game;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
